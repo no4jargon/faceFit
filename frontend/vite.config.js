fb266035b5070023e626f57830cf774d1b01200e
Anuj Shah <52823141+no4jargon@users.noreply.github.com>
2025-07-11 14:56:34 +0530

    feat: auto-select API host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://facefit-nntu.onrender.com',
+      '/api': 'http://localhost:8000',
     },
   },
 })
